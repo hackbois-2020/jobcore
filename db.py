953973e4d7c189955e788101ca7441e32ae70a08
Samuel Hunter <samuelhunter1024@gmail.com>
2020-04-19 13:34:50 -0700

    Hotpatch: Create new db connection on error

--- db.py
+++ db.py
@@ -11,31 +11,57 @@ JobDescription = namedtuple('JOBDESCRIPTION',
     'DateUpload ContractStart ContractDuration Link CompanyName')
 
 _sql = DotMap(Config.sql)
-_db = mysql.connector.connect(
-    host = _sql.server,
-    user = _sql.username,
-    passwd = _sql.password,
-    database = _sql.database,
-    ssl_ca='cert.pem'
-)
-_cursor = _db.cursor()
+
+class _Global:
+    pass
+
+def make_cursor():
+    db = mysql.connector.connect(
+        host = _sql.server,
+        user = _sql.username,
+        passwd = _sql.password,
+        database = _sql.database,
+        ssl_ca='cert.pem'
+    )
+    cursor = db.cursor()
+    _Global.db = db
+    _Global.cursor = cursor
+    return cursor
+
+def close_db():
+    try:
+        _Global.cursor.close()
+        _Global.db.close()
+    except mysql.connector.errors.DatabaseError:
+        pass
+
+make_cursor()
 
 def get_companies(length, offset):
-    _cursor.execute('SELECT * FROM `COMPANY` LIMIT %s OFFSET %s',
-                    (length, offset))
+    try:
+        _Global.cursor.execute('SELECT * FROM `COMPANY` LIMIT %s OFFSET %s',
+                               (length, offset))
 
-    return [Company(*row)._asdict() for row in _cursor.fetchall()]
+        return [Company(*row)._asdict() for row in _Global.cursor.fetchall()]
+    except mysql.connector.errors.DatabaseError as e:
+        close_db()
+        make_cursor()
+        raise e
 
 def get_jobs(company_name):
-    _cursor.execute('SELECT * FROM `JOBDESCRIPTION` WHERE CompanyName = %s',
-                    (company_name,))
+    try:
+        _Global.cursor.execute('SELECT * FROM `JOBDESCRIPTION` WHERE CompanyName = %s',
+                               (company_name,))
 
-    return [JobDescription(*row)._asdict() for row in _cursor.fetchall()]
+        return [JobDescription(*row)._asdict() for row in _Global.cursor.fetchall()]
+    except mysql.connector.errors.DatabaseError as e:
+        close_db()
+        make_cursor()
+        raise e
 
 def close_connection():
-    _db.commit()
-    _cursor.close()
-    _db.close()
+    _Global.db.commit()
+    close_db()
 
 if __name__ == '__main__':
     print(get_jobs('Skipfire'))
